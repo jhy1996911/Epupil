93a63377db5b3cebed99476d145a524fdfd375b6
jianghongyi <jianghongyi@itiger.com>
2024-10-22 16:47:08 +0800

    修改返回数据

--- ChatTimer.py
+++ ChatTimer.py
@@ -1,4 +1,6 @@
 # from Database import Database
+import json
+
 import coze
 from feishu import get_access_token, add_record
 import re
@@ -13,7 +15,7 @@ def process_summary_data(db):
             "select content from user_chat_log where role='user' and user_id=" + user_id + " order by id desc")
         user_contents = [item['content'] for item in result]
         content = "从" + str(user_contents) + (
-            "中总结用户的对话，返回信息格式为 姓名:,联系方式:,所在公司:,用户诉求:.")
+            "中总结用户的对话，返回信息格式为json格式，如 {\"姓名\":,\"联系方式\":,\"所在公司\":,\"用户诉求\":}")
         print(content)
         history = [content]
         coze_response = coze.chat(content, history, user_id)
@@ -21,11 +23,7 @@ def process_summary_data(db):
         for chunk_content in coze_response:
             content_response = content_response + chunk_content
         # print(content_response)
-        data = {}
-        data['status'] = 'Summarized'
-        condition = {}
-        condition['user_id'] = user_id
-        db.update('user_chat_log', data, condition)
+
 
         # pattern = r"姓名:(\S+) 联系方式:(\d+) 所在公司:(\S+) 用户诉求:(.+)"
         # match = re.search(pattern, content_response)
@@ -41,13 +39,14 @@ def process_summary_data(db):
         #     "所在公司": re.search(r"所在公司:\s*(\S+)", text).group(1),
         #     "用户诉求": re.search(r"用户诉求:\s*(\S+)", text).group(1)
         # }
-        result = {
-            "姓名": re.search(r"姓名:\s*(\S*)", text).group(1) if re.search(r"姓名:\s*(\S*)", text) else "",
-            "联系方式": re.search(r"联系方式:\s*(\d*)", text).group(1) if re.search(r"联系方式:\s*(\d*)", text) else "",
-            # "邮箱": re.search(r"邮箱:\s*(\S*)", text).group(1) if re.search(r"邮箱:\s*(\S*)", text) else "",
-            "所在公司": re.search(r"所在公司:\s*(\S*)", text).group(1) if re.search(r"所在公司:\s*(\S*)", text) else "",
-            "用户诉求": re.search(r"用户诉求:\s*(\S*)", text).group(1) if re.search(r"用户诉求:\s*(\S*)", text) else ""
-        }
+        result = json.loads(content_response)
+        # result = {
+        #     "姓名": re.search(r"姓名:\s*(\S*),", text).group(1) if re.search(r"姓名:\s*(\S*),", text) else "",
+        #     "联系方式": re.search(r"联系方式:\s*(\d*),", text).group(1) if re.search(r"联系方式:\s*(\d*),", text) else "",
+        #     # "邮箱": re.search(r"邮箱:\s*(\S*)", text).group(1) if re.search(r"邮箱:\s*(\S*)", text) else "",
+        #     "所在公司": re.search(r"所在公司:\s*(\S*),", text).group(1) if re.search(r"所在公司:\s*(\S*),", text) else "",
+        #     "用户诉求": re.search(r"用户诉求:\s*(\S*)", text).group(1) if re.search(r"用户诉求:\s*(\S*)", text) else ""
+        # }
 
         print("result=", result)
         if result is not None and result["联系方式"] is not None:
@@ -57,6 +56,12 @@ def process_summary_data(db):
             result["总结"] = text
             added_record = add_record(access_token, result, "tblVp9oQyqVj6Cjq")
             print("新增成功:", added_record)
+
+        data = {}
+        data['status'] = 'Summarized'
+        condition = {}
+        condition['user_id'] = user_id
+        db.update('user_chat_log', data, condition)
 
 
 print("汇总数据完成，结果已保存。")
